6d254fd771f8ca2d3c576c6174a4d2fa736d36b9
Sergio de la Torre <sergio.delatorre@unmsm.edu.pe>
2024-04-16 10:09:26 -0500

    Project setup

---
+++ src/test/java/com/bci/api/user/UserApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bci.api.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
